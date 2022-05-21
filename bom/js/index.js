b06638502136127d435c0c51e379872919d354f7
Fabrizio Carlassara <53102151+carlassaraf@users.noreply.github.com>
2022-05-21 00:59:19 -0300

    feat: add dom manipulation assignment

---
+++ bom/js/index.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    document.querySelector("#input button").addEventListener("click", addChapter);
+
+});
+
+function addChapter() {
+
+    let input = document.querySelector("#input input");
+    if(input.value) {
+
+        let p = document.createElement("p");
+        p.innerText = input.value;
+
+        let button = document.createElement("button");
+        button.innerText = "❌";
+        button.addEventListener("click", removeChapter);
+
+        let div = document.createElement("div");
+        div.setAttribute("class", "chapter");
+        div.append(p);
+        div.append(button);
+
+        document.querySelector("#chapters").append(div);
+    }
+
+}
+
+function removeChapter() {
+
+    this.parentElement.remove();
+}
