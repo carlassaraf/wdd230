ab63874de9d9dbdf4c6ee63e401db84032c7b8b6
Fabrizio <fabrizio.carlassara@gmail.com>
2023-01-24 23:30:39 -0300

    add date dinamically

--- chamber/scripts/index.js
+++ chamber/scripts/index.js
@@ -1,10 +1,44 @@
+/**
+ * Date prototype to get the day as String
+ * @returns String
+ */
+Date.prototype.getDayString = function() {
+
+    // Array with every day of the week as a string
+    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    // Return the proper day as string with today's index (0-6)
+    return days[this.getDay()];
+}
+
+/**
+ * Date prototype to get the month as String
+ * @returns String
+ */
+Date.prototype.getMonthString = function() {
+
+    // Array with every month
+    const months = [
+        "January", "February", "March", "April",
+        "May", "June", "July", "August",
+        "September", "October", "November", "December"
+    ];
+    // Return month as string
+    return months[this.getMonth()];
+}
+
 document.addEventListener("DOMContentLoaded", () => {
 
     // Hamburger menu
-    let navLinks = document.querySelector(".nav-links");
-    let menu = document.querySelector("#menu");
+    const navLinks = document.querySelector(".nav-links");
+    const menu = document.querySelector("#menu");
+    // Current date paragraph
+    const date = document.querySelector("#date-section p");
+    // Date object
+    const dateObj = new Date();
 
     // Add event for menu click
     menu.addEventListener("click", () => navLinks.classList.toggle("responsive-menu"));
 
+    // Add current date
+    date.innerText = `${dateObj.getDayString()}, ${dateObj.getDate()} ${dateObj.getMonthString()} ${dateObj.getFullYear()}`;
 });
