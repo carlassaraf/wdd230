b2e4ad43b08b14639d96faac82f4024dd02e980b
Fabrizio <fabrizio.carlassara@gmail.com>
2023-03-09 20:47:55 -0300

    add icon and weather status

--- lesson10/scripts/weatherapi.js
+++ lesson10/scripts/weatherapi.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", () => {
     // API url
     const url = "https://api.openweathermap.org/data/2.5/weather?q=Fairbanks&units=imperial&appid=51c9e7e908aa3baccb967c02e9950992";
     // Fetch API
-    apiFetch(url).then(data => displayResults(currentTemp, data));
+    apiFetch(url).then(data => displayResults(currentTemp, weatherIcon, captionDesc, data));
 });
 
 async function apiFetch(url) {
@@ -32,7 +32,29 @@ async function apiFetch(url) {
     return data;
 }
 
-function displayResults(currentTemp, weatherData) {
+function displayResults(currentTemp, weatherIcon, captionDesc, weatherData) {
     // Get the temperature in Fahrenheit without decimals
     currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
+    // Create url for icon
+    const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+    // Create alt text
+    const desc = weatherData.weather[0].description.capitalize();
+    // Set properties
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = desc;
+}
+
+String.prototype.capitalize = function() {
+    // split the string by spaces
+    let arr = this.split(" ");
+    // capitalized array
+    let capArr = [];
+    // loop through every word
+    arr.forEach(word => {
+        // capitalize word
+        capArr.push(`${word.slice(0, 1).toUpperCase()}${word.slice(1)}`);
+    });
+    // join array and return
+    return capArr.join(" ");
 }
