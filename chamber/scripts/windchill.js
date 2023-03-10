92cc836d70a0d8b65f70b9f3da8ba14b930f199f
Fabrizio <fabrizio.carlassara@gmail.com>
2023-03-09 21:12:58 -0300

    use weather api

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -35,6 +35,10 @@ function getWindChill(temperature, windSpeed) {
 }
 
 document.addEventListener("DOMContentLoaded", () => {
+    // API url
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=Quilmes&units=metric&appid=51c9e7e908aa3baccb967c02e9950992";
+    // Fetch API and display
+    apiFetch(url).then(data => displayResults(data));
     // Get temperature value
     const temperature = toFahrenheit(parseFloat(document.querySelector("#temperature").innerText));
     // Get wind speed value
@@ -44,3 +48,72 @@ document.addEventListener("DOMContentLoaded", () => {
     // Set value
     document.querySelector("#wind-chill span").innerText = isNaN(windChill)? "N/A" : `${toCelsius(windChill).toFixed(1)} °C`;
 });
+
+/**
+ * Fetches the data from given url
+ * @param {String} url API request url
+ * @returns JSON data object
+ */
+async function apiFetch(url) {
+    // JSON data
+    let data;
+
+    try {
+        // Fetch url
+        const response = await fetch(url);
+        // Verify response
+        if (response.ok) {
+        // Get JSON data
+        data = await response.json();
+        // Catch error
+        } else {
+            throw Error(await response.text());
+        }
+    // Catch error
+    } catch (error) {
+        console.log(error);
+        return null;
+    }
+    return data;
+}
+
+/**
+ * Properly displays the weather info
+ * @param {Object} weatherData JSON data object
+ */
+function displayResults(weatherData) {
+    // select HTML elements in the document
+    const currentTemp = document.querySelector('#temperature');
+    const weatherIcon = document.querySelector('#temperature-div img');
+    const captionDesc = document.querySelector('#weather-status h3');
+    const windspeed = document.querySelector('#wind-speed span');
+    // Get the temperature in Fahrenheit without decimals
+    currentTemp.innerText = `${weatherData.main.temp.toFixed(0)} °C`;
+    // Create url for icon
+    const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+    // Create alt text
+    const desc = weatherData.weather[0].description.capitalize();
+    // Set properties
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = desc;
+    windspeed.innerText = `${weatherData.wind.speed} km/h`;
+}
+
+/**
+ * Returns the same capitalized string
+ * @returns Capitalized String
+ */
+String.prototype.capitalize = function() {
+    // split the string by spaces
+    let arr = this.split(" ");
+    // capitalized array
+    let capArr = [];
+    // loop through every word
+    arr.forEach(word => {
+        // capitalize word
+        capArr.push(`${word.slice(0, 1).toUpperCase()}${word.slice(1)}`);
+    });
+    // join array and return
+    return capArr.join(" ");
+}
